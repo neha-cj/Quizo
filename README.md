4a5eda4b4da9f6b3be2dfdd1495bd64bae502382
Neha C J <cjneha890@gmail.com>
2025-06-25 11:23:58 +0530

    overlay result

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 
 A dynamic quiz web application built with HTML, CSS, and JavaScript. 
 
-
+-a quiz application that allows users to answer a series of multiple-choice questions. 
+-The questions appear one at a time, and each should have several options to choose from. 
+-The user can select their answer and then click next button to move to the next question. 
+-There is a countdown timer for each question, and if the time runs out, the quiz will automatically move to the next one.
+- At the end of the quiz, the user can see a summary of their performance, including how many questions they got right and a review of their answers. 
+-The final result is shown in a pop-up or overlay window.
--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const questiondiv =document.querySelector(".question p");
 const optionsdiv=document.querySelector(".options")
 const timep=document.querySelector(".time-left");
 const nextBtn= document.querySelector(".next-btn");
-const progressDiv= document.querySelector(".progress")
+const progressDiv= document.querySelector(".progress");
+const reviewCard=document.querySelector(".review-section");
 
 let currentQ=0
 let score=0
@@ -41,7 +42,7 @@ function loadQuestion(){
 
         button.addEventListener("click",() =>{
             selectedOp=option;
-              document.querySelectorAll(".option-button").forEach(btn => {
+            document.querySelectorAll(".option-button").forEach(btn => {
                 btn.classList.remove("selected");
             });
             button.classList.add("selected");   
@@ -52,7 +53,7 @@ function loadQuestion(){
 }
 
 function startTimer(){
-    //clearInterval(timer);
+    clearInterval(timer);
     timer= setInterval(() =>{
         timeleft--;
         timep.textContent=`${timeleft}s`;
@@ -63,16 +64,18 @@ function startTimer(){
             currentQ++;
             if (currentQ < quizData.length) {
                 loadQuestion();
-            } else {
-                showFinalResult();
             }
         }
     },1000);
 }
 
 nextBtn.addEventListener("click",() => {
+    const q = quizData[currentQ];
     if (selectedOp!==null){
-        userAns.push(selectedOp)
+        if (selectedOp==q.correct){
+            score++;
+        }
+        userAns.push(selectedOp)      
     }
     else{
         userAns.push(null);
@@ -87,12 +90,29 @@ nextBtn.addEventListener("click",() => {
 })
 
 function showFinalResult(){
+   
     console.log(userAns)
     document.querySelector(".overlay").style.display="block";
     document.querySelector(".overlay-body p").textContent =
         `You attempted ${userAns.filter(ans => ans !== null).length} out of ${quizData.length} questions.`;
+    document.getElementById("score").textContent=
+        `you scored: ${score} out of ${quizData.length}`;
+    
+    quizData.forEach((q,index)=>{
+        const userAnswer = userAns[index];
+        const isCorrect = userAnswer === q.correct;
+
+        const p = document.createElement("p");
+        p.innerHTML = `
+            <strong>Q${index + 1}.</strong> ${q.question}<br>
+            <strong>Options:</strong> <br>${q.options.join("<br>")}<br><br>
+            <strong>Correct Answer:</strong> ${q.correct}<br>
+            <strong>Your Answer:</strong> ${userAnswer === null ? "Not Answered" : userAnswer}
+            <br><span style="color:${isCorrect ? 'green' : 'red'};">${isCorrect ? " Correct" : " Incorrect"}</span><br><br>`;
+        reviewCard.appendChild(p);
+    })
 }
 
 function closeOverlay(){
     document.querySelector(".overlay").style.display="none";
-}
+} 
